9b46f9a44cbc8538a719f9f086ba7247deb0f5e4
Josh Ballanco <jballanc@gmail.com>
2014-02-11 01:31:18 +0200

    Added a nice implementation of modo...
    ...but this does break fizzbuzz for the moment.

--- src/logicbuzz/core.clj
+++ src/logicbuzz/core.clj
@@ -3,42 +3,29 @@
             [clojure.core.logic.fd :as fd])
   (:gen-class))
 
-(cl/defne fizzo
-  "Succeeds if x is evenly divisible by 3"
-  [x l]
-  ([3 "fizz"])
-  ([x "fizz"]
+(cl/defne modo
+  "Unifies r with the remainder after dividing p by q"
+  [p q r]
+  ([p _ p]
+   (fd/< p q))
+  ([_ _ r]
    (cl/fresh [a]
-             (fd/in a (fd/interval 3 100))
-             (fd/+ a 3 x)
-             (fizzo a l))))
-
-(cl/defne buzzo
-  "Succeeds if x is evenly divisible by 5"
-  [x l]
-  ([5 "buzz"])
-  ([_ "buzz"]
-   (cl/fresh [a]
-             (fd/in a (fd/interval 5 100))
-             (fd/+ a 5 x)
-             (buzzo a l))))
+             (fd/+ a q p)
+             (modo a q r))))
 
 (cl/defnu fizzbuzzo
   "- If x is divisible by 3, unifies with 'fizz'
    - If x is divisible by 5, unifies with 'buzz'
    - If x is divisible by 3 and 5, unifies with 'fizzbuzz'
-   - Otherwise unifies with x"
+   - Otherwise x unifies with itself"
   [x l]
   ([_ "fizzbuzz"]
-   (cl/fresh [a b]
-             (fizzo x a)
-             (buzzo x b)))
+   (modo x 3 0)
+   (modo x 5 0))
   ([_ "fizz"]
-   (cl/fresh [a]
-             (fizzo x a)))
+   (modo x 3 0))
   ([_ "buzz"]
-   (cl/fresh [a]
-             (buzzo x a)))
+   (modo x 5 0))
   ([_ x]))
 
 (defn fizzbuzz
